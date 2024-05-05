bfda19ad0fa895a10a7fc0b4e2de770fa3270131
debora berte <arobedbee@gmail.com>
2024-05-05 09:55:28 -0300

    feat: create user entity

---
+++ lib/amigo_safe/accounts/user.ex
@@ -0,0 +1,18 @@
+defmodule AmigoSafe.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :contact, :string
+    has_many :amigos, AmigoSafe.Pets.Amigo
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :contact])
+    |> validate_required([:name, :contact])
+  end
+end
